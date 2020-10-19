e56b3b4ec6817c05c2649f9a25ac0198d7f0051e
Rhonda04 <rpoole10@icloud.com>
2020-10-19 22:47:55 +0000

    Variable B

--- index.java
+++ index.java
@@ -1,10 +1,16 @@
 void setup() {
-	size(500, 500);
+	size(300, 300);
+  
 }
-int x = 250;
-int y = 200;
+int x = 145;
+int y = 150;
 int d = 100;
 void draw() {
+
+int x = int(random(1,500));
+int y = int(random(1,500));
+
+
 // Step 1: Write code that draws the following screenshots 
 // with hard-coded values. (Feel free to use colors 
 // instead of grayscale.)
@@ -18,15 +24,20 @@ void draw() {
 
 // Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
 
-  background(175);
+  
   stroke(0);
-  fill(255);
+  
+  int r = (int) random(0, 255);
+  int g = (int) random(0, 255);
+  int b = (int) random(0, 255);
+
+  fill( r, g, b)
 
    ellipse(x, y, d,d)
-   x--;
-   y--;
+
+   
 }
 
-void mousePressed(){
+ void mousePressed(){
 x = x-1
-}
+ }
